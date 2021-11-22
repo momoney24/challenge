b73e8e92070c6f05b74071c788d002a93d398068
momoney24 <47685919+momoney24@users.noreply.github.com>
2021-11-22 12:20:10 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 'use strict';
 const express = require('express');
 const app = express();
-const registerRoutes = require('./routes');
+const registerRoutes = require('./src/routes');
+const path = require('path');
 
 // server config
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
+app.use(express.static(__dirname + '/public'))
+app.get('/' , (req,res)=>{
+   // res.sendFile(path.join(__dirname,"public" ,"index.html"))
+   res.sendFile(path.join(__dirname,"public" ,"index.html"))
+})
 // register routes
 registerRoutes(app);
 
